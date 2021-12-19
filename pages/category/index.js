26454d8cfc78af3dcd498960d77c533220bc21fa
ApiwatHantrakool <apiwat@simyasolutions.com>
2021-12-20 00:04:48 +0700

    Navigating programmatically

--- pages/category/index.js
+++ pages/category/index.js
@@ -1,3 +1,17 @@
+import { useRouter } from 'next/router';
+
 export default function Category() {
-  return <div>Category</div>;
+  const router = useRouter();
+  const goToSubCategoryItem = () => {
+    router.push({
+      pathname: '/category/[id]/[subCategoryId]',
+      query: { id: 'category01', subCategoryId: 'sub01' },
+    });
+  };
+  return (
+    <>
+      <h1>Category</h1>
+      <button onClick={goToSubCategoryItem}>{'Sub Category item'}</button>
+    </>
+  );
 }
