1e0cf95433581301bf61a4ea0cc6d87613fdad0d
ApiwatHantrakool <apiwat@simyasolutions.com>
2021-12-20 23:35:37 +0700

    Image optimization

--- pages/category/[id]/index.js
+++ pages/category/[id]/index.js
@@ -1,3 +1,17 @@
+import Image from 'next/image';
+
 export default function Subcategory() {
-  return <div>Subcategory</div>;
+  return (
+    <div>
+      <div>Subcategory</div>
+      <Image
+        src={'https://drive.google.com/uc?id=1WXD-LZtiuwrs25Bok9sgdPeMucAnH_8D'}
+        alt={'test image'}
+        width={1200}
+        height={1200}
+        objectFit={'cover'}
+        layout={'responsive'}
+      />
+    </div>
+  );
 }
