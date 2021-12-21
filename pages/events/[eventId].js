a2544ba72b6d5192c4d149a5b6f4ff694bfc3b56
ApiwatHantrakool <apiwat@simyasolutions.com>
2021-12-21 19:27:42 +0700

    Provide event pages

---
+++ pages/events/[eventId].js
@@ -0,0 +1,7 @@
+export default function EventDetailPage() {
+  return (
+    <div>
+      <h1>EventDetailPage</h1>
+    </div>
+  );
+}
---
+++ pages/events/index.js
@@ -0,0 +1,7 @@
+export default function AllEventsPage() {
+  return (
+    <div>
+      <h1>AllEventsPage</h1>
+    </div>
+  );
+}
