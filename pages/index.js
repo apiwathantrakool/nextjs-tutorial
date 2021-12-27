da2f2523cfcb7bbe17fa77ed2cfe61532cdfccaf
ApiwatHantrakool <apiwat@simyasolutions.com>
2021-12-27 23:07:29 +0700

    Basic getStaticProps

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,29 @@
-import { getFeaturedEvents } from '../dummy-data';
+import { getFeaturedEvents } from '../data/dummy-data';
 import EvenList from '../components/events/event-list';
 
-export default function HomePage() {
+export default function HomePage(props) {
+  const { products } = props;
   const featuredEvents = getFeaturedEvents();
   return (
     <div>
       <h1>Home page</h1>
+      <div>Data from hardcode</div>
       <EvenList itemList={featuredEvents} />
+      <div>Data from getStaticProps</div>
+      <EvenList itemList={products} />
     </div>
   );
+}
+
+export async function getStaticProps() {
+  return {
+    props: {
+      products: [
+        {
+          id: 'e1',
+          title: 'Test getStaticProps',
+        },
+      ],
+    },
+  };
 }
