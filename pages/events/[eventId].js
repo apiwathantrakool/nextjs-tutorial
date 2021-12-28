1ea548e7c53e9480fb39e86c5e5a7608266c0945
ApiwatHantrakool <apiwat@simyasolutions.com>
2021-12-28 20:38:17 +0700

    Static generation: Get params

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,7 +1,36 @@
-export default function EventDetailPage() {
+export default function EventDetailPage(props) {
+  const { params } = props;
+  console.log('!!!!params: ', params);
   return (
     <div>
       <h1>EventDetailPage</h1>
     </div>
   );
+}
+
+export async function getStaticProps(context) {
+  const { params } = context;
+  return {
+    props: {
+      params,
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  return {
+    paths: [
+      {
+        params: {
+          eventId: '01',
+        },
+      },
+      {
+        params: {
+          eventId: '02',
+        },
+      },
+    ],
+    fallback: false,
+  };
 }
